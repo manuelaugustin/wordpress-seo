10ef911ac2bd8a45734b165dfa6885341c098071
Manuel <manuel@yoast.com>
2019-12-05 12:32:42 +0100

    Implement update readme part 1

--- grunt/config/update-readme.js
+++ grunt/config/update-readme.js
@@ -1,3 +1,26 @@
+class VersionNumber {
+	constructor( versionNumberString ) {
+		this.versionNumberString = versionNumberString;
+	}
+
+	versionNumber( ) {
+		const versionNumberString = this.versionNumberString;
+
+		const versionNumber = ( /(\d+).(\d+).?(\d+)?/g ).exec( versionNumberString );
+
+		// return versionNumber;
+		return {
+			major: parseInt( versionNumber[1] ),
+			minor: parseInt( versionNumber[2] ),
+			patch: parseInt( versionNumber[3] ) || 0,
+		}
+	}
+
+	isPatch( ) {
+		return this.versionNumber().patch > 0;
+	}
+}
+
 /**
  * ...
  *
@@ -9,6 +32,19 @@ module.exports = function( grunt ) {
 		"update-readme",
 		"Prompts the user for the changelog entries and updates the readme.txt",
 		function() {
+			let changelog = grunt.file.read( "./readme.txt" );
+			let newVersion = grunt.option('plugin-version');
+			let newChangelog = grunt.option('new-changelog');
+
+			const versionNumber = new VersionNumber( newVersion );
+
+			if( !versionNumber.isPatch() ) {
+				const releaseInChangelog = /= \d+\.\d+(\.\d+)? =/g;
+				const allReleasesInChangelog = changelog.match( releaseInChangelog );
+			}
+
+
+			changelog = changelog.replace( /== Changelog ==/ig, "== Changelog ==\n\n" + newChangelog );
 		}
 	);
 };
