36cf5b098a43d6c18e0c24326b129ef6e17c7f1f
Manuel <manuel@yoast.com>
2019-12-05 13:32:50 +0100

    Implement update readme part 2

--- grunt/config/update-readme.js
+++ grunt/config/update-readme.js
@@ -1,13 +1,13 @@
 class VersionNumber {
 	constructor( versionNumberString ) {
 		this.versionNumberString = versionNumberString;
+		this.major = this.versionNumber().major;
+		this.minor = this.versionNumber().minor;
+		this.patch = this.versionNumber().patch;
 	}
-
 	versionNumber( ) {
 		const versionNumberString = this.versionNumberString;
-
 		const versionNumber = ( /(\d+).(\d+).?(\d+)?/g ).exec( versionNumberString );
-
 		// return versionNumber;
 		return {
 			major: parseInt( versionNumber[1] ),
@@ -15,12 +15,10 @@ class VersionNumber {
 			patch: parseInt( versionNumber[3] ) || 0,
 		}
 	}
-
 	isPatch( ) {
 		return this.versionNumber().patch > 0;
 	}
 }
-
 /**
  * ...
  *
@@ -35,16 +33,23 @@ module.exports = function( grunt ) {
 			let changelog = grunt.file.read( "./readme.txt" );
 			let newVersion = grunt.option('plugin-version');
 			let newChangelog = grunt.option('new-changelog');
-
 			const versionNumber = new VersionNumber( newVersion );
-
 			if( !versionNumber.isPatch() ) {
 				const releaseInChangelog = /= \d+\.\d+(\.\d+)? =/g;
 				const allReleasesInChangelog = changelog.match( releaseInChangelog );
+				const sanitizedVersionNumbers = allReleasesInChangelog.map( element => new VersionNumber(element.slice( 2, element.length- 2) ) );
+				const highestMajor = Math.max( ...sanitizedVersionNumbers.map( sanitizedVersionNumber => sanitizedVersionNumber.major ) );
+				const lowestMajor = Math.min( ...sanitizedVersionNumbers.map( sanitizedVersionNumber => sanitizedVersionNumber.major ) );
+				if ( highestMajor !== lowestMajor ) {
+					changelog = changelog.replace( new RegExp( "= " + lowestMajor + "(.|\\n)*= Earlier versions =" ), "= Earlier versions =" );
+				} else {
+					const lowestMinor = Math.min( ...sanitizedVersionNumbers.map( sanitizedVersionNumber => sanitizedVersionNumber.minor ) );
+					const lowestVersion = `${lowestMajor}.${lowestMinor}`;
+					changelog = changelog.replace( new RegExp( "= " + lowestVersion + "(.|\\n)*= Earlier versions =" ), "= Earlier versions =" );
+				}
 			}
-
-
 			changelog = changelog.replace( /== Changelog ==/ig, "== Changelog ==\n\n" + newChangelog );
+			// save changelog
 		}
 	);
 };
